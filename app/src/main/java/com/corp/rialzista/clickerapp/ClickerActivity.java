30b1bac737c6150ad1078c5c5bce41e967723348
Rialzista <kostya.mihailov@gmail.com>
2015-01-19 07:33:40 +0300

    Add base functional

--- app/src/main/java/com/corp/rialzista/clickerapp/ClickerActivity.java
+++ app/src/main/java/com/corp/rialzista/clickerapp/ClickerActivity.java
@@ -1,7 +1,6 @@
 package com.corp.rialzista.clickerapp;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class ClickerActivity extends ActionBarActivity {
@@ -48,19 +48,75 @@ public class ClickerActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener{
+
+        private TextView mRemoveTV, mAddTV, mCounterTV;
+        private ImageView mRefresh;
 
         public PlaceholderFragment() {
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.removeTV:
+                    this.decCounter();
+                    break;
+                case R.id.addTV:
+                    this.incCounter();
+                    break;
+                case R.id.refreshCounter:
+                    this.resetCounter();
+                    break;
+            }
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_clicker, container, false);
+
+            this.mRemoveTV = (TextView) rootView.findViewById(R.id.removeTV);
+            this.mAddTV = (TextView) rootView.findViewById(R.id.addTV);
+            this.mCounterTV = (TextView) rootView.findViewById(R.id.clickCount);
+
+            this.mRefresh = (ImageView) rootView.findViewById(R.id.refreshCounter);
+
+            this.mRemoveTV.setOnClickListener(this);
+            this.mAddTV.setOnClickListener(this);
+            this.mRefresh.setOnClickListener(this);
+
             return rootView;
         }
+
+        private void incCounter() {
+            String sCounter = this.mCounterTV.getText().toString();
+            try {
+                int value = Integer.parseInt(sCounter);
+                this.mCounterTV.setText(++value + "");
+            } catch (Exception ex) {
+                System.out.println("Could not parse " + ex);
+            }
+        }
+
+        private void decCounter() {
+            String sCounter = this.mCounterTV.getText().toString();
+            try {
+                int value = Integer.parseInt(sCounter);
+                this.mCounterTV.setText(--value + "");
+            } catch (Exception ex) {
+                System.out.println("Could not parse " + ex);
+            }
+        }
+
+        private void resetCounter() {
+            this.mCounterTV.setText("0");
+        }
     }
+
+
 }
